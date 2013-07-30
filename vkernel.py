95e71bce5ac1268cd54b18f075533c8118c1f66c
JesseLivezey <jesse.livezey@gmail.com>
2013-07-30 16:02:13 -0700

    Added options for using symmetric second order term

--- vkernel.py
+++ vkernel.py
@@ -49,15 +49,48 @@ def get_vone(stimuli,outputs,guess=None,meth=None):
         output = opt.minimize(vonef(stimuli,outputs),guess,method = meth,jac=vonef_grad(stimuli,outputs)).x
     return output
 
+#Function to take matrix and return array of symmetrix elements
+def symm(matrix):
+    length = matrix.shape[0]
+    output = np.zeroes((length**2+length)/2)
+    outii = 0
+    for ii in xrange(length):
+        for jj in xrange(ii+1):
+            output[outii] = matrix[ii,jj]
+            outii += 1
+    return output
+#Function to take an array of symmetric elements and return a symmetrix matrix
+def unsymm(array):
+    length = array.shape[0]
+    lengthMat = (int(round(np.sqrt(8*length+1)))-1)/2
+    output = np.zeros((lengthMat,lengthMat))
+    outii = 0
+    for ii in xrange(lengthMat):
+        for jj in xrange(ii+1):
+            output[ii,jj] = array[outii]
+            output[jj,ii] = array[outii]
+            outii += 1
+    return output
 #Function that calculates response from 2nd order Volterra model
 def vtwo(stimulus,kernels):
     lengths = stimulus.shape[0]
     one = kernels[1:1+lengths]
     two = np.reshape(kernels[1+lengths:],(lengths,lengths))
     return kernels[0]+np.dot(stimulus,kernels[1:1+lengths])+np.dot(stimulus,np.dot(two,stimulus))
+#Function that calculates response from 2nd order Volterra model, given symmetric kernel
+def vtwoS(stimulus,kernels):
+    lengths = stimulus.shape[0]
+    one = kernels[1:1+lengths]
+    two = np.reshape(unsymm(kernels[1+lengths:]),(lengths,lengths))
+    return kernels[0]+np.dot(stimulus,kernels[1:1+lengths])+np.dot(stimulus,np.dot(two,stimulus))
 #Returns vtwo function to be minimized based on given stimuli and measured response
 def vtwof(stimuli,outputs):
     func = lambda kernels: (.5/outputs.shape[0])*np.sum(np.array([(outputs[i]-vtwo(stimuli[i],kernels))**2
+                                                                  for i in xrange(outputs.shape[0])]))
+    return func
+#Returns vtwo function to be minimized based on given stimuli and measured response
+def vtwofS(stimuli,outputs):
+    func = lambda kernels: (.5/outputs.shape[0])*np.sum(np.array([(outputs[i]-vtwoS(stimuli[i],kernels))**2
                                                                   for i in xrange(outputs.shape[0])]))
     return func
 #Returns gradient of vtwo function
@@ -72,10 +105,39 @@ def vtwof_grad(stimuli,outputs):
                                            np.array([gradhone(k,kernels) for k in xrange(stimuli[0].shape[0])]),
                                            np.array([[gradhtwo(n,m,kernels) for m in xrange(stimuli[0].shape[0])] for n in xrange(stimuli[0].shape[0])]).flatten()))
     return func 
+#Returns coefficient for diagonal or off-diagonal terms
+def oneOrTwo(n,m):
+    if n == m:
+        output = 1
+    else:
+        output = 2
+    return output
+#Returns gradient of vtwo function
+def vtwof_gradS(stimuli,outputs):
+    gradhzero = lambda kernels: (-1./outputs.shape[0])*np.sum(np.array([(outputs[i]-vtwoS(stimuli[i],kernels))
+                                                                         for i in xrange(outputs.shape[0])]))
+    gradhone = lambda k,kernels: (-1./outputs.shape[0])*np.sum(np.array([(stimuli[i][k]*(outputs[i]-vtwoS(stimuli[i],kernels)))
+                                                                          for i in xrange(outputs.shape[0])]))
+    gradhtwo = lambda n,m,kernels:(-1./outputs.shape[0])*oneOrTwo(n,m)*np.sum(np.array([(stimuli[i][n]*stimuli[i][m]*(outputs[i]-vtwoS(stimuli[i],kernels)))
+                                                                           for i in xrange(outputs.shape[0])]))
+    func = lambda kernels: np.concatenate((np.array([gradhzero(kernels)]),
+                                           np.array([gradhone(k,kernels) for k in xrange(stimuli[0].shape[0])]),
+                                           np.array([[gradhtwo(n,m,kernels) for m in xrange(n+1)] for n in xrange(stimuli[0].shape[0])]).flatten()))
+    return func 
 #Function that does minimization to find vtwo.
 def get_vtwo(stimuli,outputs,guess=None,meth=None):
     if guess is None:
         guess = np.zeros(1+stimuli[0].shape[0]+stimuli[0].shape[0]**2)
+    if meth is None:
+        output = opt.minimize(vtwof(stimuli,outputs),guess,jac=vtwof_grad(stimuli,outputs)).x
+    else:
+        output = opt.minimize(vtwof(stimuli,outputs),guess,method = meth,jac=vtwof_grad(stimuli,outputs)).x
+    return output
+#Function that does minimization to find vtwo.
+def get_vtwoS(stimuli,outputs,guess=None,meth=None):
+    length = stimuli[0].shape[0]
+    if guess is None:
+        guess = np.zeros(1+length+(length**2+length)/2)
     if meth is None:
         output = opt.minimize(vtwof(stimuli,outputs),guess,jac=vtwof_grad(stimuli,outputs)).x
     else:
@@ -91,7 +153,7 @@ def vresponse(stimuli,kernels):
         vker = vzero
     elif lengthk == 1+lengths:
         vker = vone
-    elif lengthk == 1+lengths+lengths**2:
+    elif lengthk > 1+lengths:
         vker = vtwo
     for i,stimulus in enumerate(stimuli):
         response[i] = vker(stimulus,kernels)
