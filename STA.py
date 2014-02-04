fixed normalization in stc

STA.py
@@ -79,7 +79,7 @@ def sparseSTASys(stimuli,outputs,whitened=None):
     return strfs.T
 
 def STC(stimuli,outputs,meancov):
-    stcM = np.mean(np.array([np.outer(stimuli[ii],stimuli[ii])*outputs[ii] for ii in xrange(outputs.shape[0])]),axis=0)/np.mean(outputs)
+    stcM = np.sum(np.array([np.outer(stimuli[ii],stimuli[ii])*outputs[ii] for ii in xrange(outputs.shape[0])]),axis=0)/np.mean(outputs)
     stcM = stcM-meancov
     return stcM
 